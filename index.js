20047191d11257324c5b773b063000142354a09f
Markus Dopp <markusdopp@gmail.com>
2018-10-18 15:03:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ constructor(props){
     this.data = [
       {time: '09:00', title: 'Archery Training', description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ', circleColor: '#009688',lineColor:'#009688'},
       {time: '10:45', title: 'Play Badminton', description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.'},
-      {time: '12:00', title: 'Nüsse', description: contents},
+      {time: '12:00', title: 'Eat something', description: contents},
       {time: '14:00', title: 'Play Tennis', description: 'Team sport played between two teams of eleven players with a spherical ball. ',lineColor:'#009688'},
       {time: '16:30', title: 'Go to Fitness center', description: 'Look out for the Best Gym & Fitness Centers around me :)', circleColor: '#009688'}
     ]
